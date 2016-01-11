88c664fbc74178ea4175305ad65362c130a7aeec
Slava Petrov <slavik--petrov@yandex.ru>
2016-01-11 12:31:43 +0300

    Create migration to seed database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160111074137) do
+ActiveRecord::Schema.define(version: 20160111080229) do
 
   create_table "products", force: :cascade do |t|
     t.string   "title"

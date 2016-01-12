c4e05e6b9dd3c1ed8e482e3e46370721f2bce25c
Slava Petrov <slavik--petrov@yandex.ru>
2016-01-12 15:36:56 +0300

    Parse orders input

--- app.rb
+++ app.rb
@@ -18,6 +18,26 @@ get '/about' do
 	erb :about
 end
 
+def parse_order order_string
+
+	s1 = order_string.split(',')	
+	arr = []
+	s1.each do |x|
+		s2 = x.split('=')
+		s3 = s2[0].split('_')
+		id = s3[1]
+		cnt = s2[1]
+		arr2 = [id, cnt]
+		arr.push arr2
+	end
+
+	return arr
+
+end
+
 post '/cart' do
-  erb 'Hello!'
+	orders = params[:orders]
+	orders_arr = parse_order orders	
+	
+  erb "Pizza #{orders_arr}"
 end

94747ead03a55ecfd82a599e1b2209e5cc38d50d
Slava Petrov <slavik--petrov@yandex.ru>
2016-01-13 10:11:34 +0300

    Update orders migration

--- db/migrate/20160113064240_create_orders.rb
+++ db/migrate/20160113064240_create_orders.rb
@@ -1,11 +1,11 @@
-class AddOrders < ActiveRecord::Migration
+class CreateOrders < ActiveRecord::Migration
   def change
   	create_table :orders do |t|
   		t.text :name
   		t.text :phone
   		t.text :address
   		t.text :orders_input
-  		
+
   		t.timestamps
   	end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160111080229) do
+ActiveRecord::Schema.define(version: 20160113064240) do
+
+  create_table "orders", force: :cascade do |t|
+    t.text     "name"
+    t.text     "phone"
+    t.text     "address"
+    t.text     "orders_input"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "products", force: :cascade do |t|
     t.string   "title"
